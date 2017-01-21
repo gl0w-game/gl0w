0ba6f41e32fe917b3b2459317fc82f510c25a623
mariano.aquino <mariano.aquino@gmail.com>
2017-01-21 06:54:14 -0300

    restructured variables and functions

--- main.lua
+++ main.lua
@@ -1,74 +1,162 @@
 maintimer = 0
 local clock = os.clock
 
-function sleep(n)
-    local t0 = clock()
-    while clock() - t0 <= n do
-    end
-end
+--====================================
 
 width = love.graphics.getWidth()
 height = love.graphics.getHeight()
-offset_y={300,400,500}
-amplitude={100,150,125}
-speed = 2
-freq = 0.05
-colors = {red, green, blue}
-altura = {100, 100, 100}
+
+--====================================
+
+--COLORS
+
+colors = {}
+colors["red"] = {}
+colors["red"]["r"] = 255
+colors["red"]["g"] = 0
+colors["red"]["b"] = 0
+colors["red"]["a"] = 127
+
+colors["green"] = {}
+colors["green"]["r"] = 0
+colors["green"]["g"] = 255
+colors["green"]["b"] = 0
+colors["green"]["a"] = 127
+
+colors["blue"] = {}
+colors["blue"]["r"] = 0
+colors["blue"]["g"] = 0
+colors["blue"]["b"] = 255
+colors["blue"]["a"] = 127
+
+RED = colors["red"]
+GREEN = colors["green"]
+BLUE = colors["blue"]
+
+function set_color(color)
+    love.graphics.setColor(color["r"], color["g"], color["b"], color["a"])
+end
+
+--====================================
+
+-- WAVES
+
+waves = {}
+waves["red"] = {}
+waves["red"]["color"] = RED
+waves["red"]["offset_x"] = 170
+waves["red"]["offset_y"] = 0
+waves["red"]["speed"] = 7
+waves["red"]["freq"] = 0.05
+waves["red"]["amplitude"] = 80
+
+waves["green"] = {}
+waves["green"]["color"] = GREEN
+waves["green"]["offset_x"] = 300
+waves["green"]["offset_y"] = 60
+waves["green"]["speed"] = 7
+waves["green"]["freq"] = 0.05
+waves["green"]["amplitude"] = 120
+
+waves["blue"] = {}
+waves["blue"]["color"] = BLUE
+waves["blue"]["offset_x"] = 520
+waves["blue"]["offset_y"] = 0
+waves["blue"]["speed"] = 7
+waves["blue"]["freq"] = 0.05
+waves["blue"]["amplitude"] = 180
+
+function draw_wave_point(pos_y, offset_x, offset_y, speed, freq, amplitude, color, radius)
+    set_color(color)
+    pos_x = wave_point(pos_y, speed, freq, amplitude)
+    love.graphics.circle( "fill", pos_x + offset_x, pos_y + offset_y, radius)
+end
+
+function wave_point(pos_y, speed, freq, amplitude)
+    return math.sin(maintimer * -speed + freq * pos_y) * amplitude
+end
+
+function change_wave_offset_y(wave, inc)
+    waves[wave]["offset_y"] = waves[wave]["offset_y"] + inc
+end
+
+--====================================
+
+--PLAYER
+
+LAST_STATE = 3
+
+player = {}
+player["pos_y"] = 300
+player["color"] = "red"
+
+function change_player_color()
+    if player["color"] == "red" then
+        player["color"] = "green"
+    elseif player["color"] == "green" then
+        player["color"] = "blue"
+    elseif player["color"] == "blue" then
+        player["color"] = "red"
+    end
+end
+
+--========================================================
+
+-- GAME UPDATE
+
+-- para evitar inputs multiples cuando se mantiene apretada una tecla
+
+keysdown = 0
+
+function love.keypressed(key, scancode, isrepeat)
+    if key == "space" and keysdown == 0 then
+        change_player_color()
+        keysdown = keysdown + 1
+    end
+end
+
+function love.keyreleased(key, scancode)
+    keysdown = 0
+end
 
 function love.update(dt)
     maintimer = maintimer + dt
 
-    altura[1] = altura[1] + (freq * speed/4 * amplitude[1] * 20 * dt)
-    altura[2] = altura[2] + (freq*2 * speed/4 * amplitude[2] * 20 * dt)
-    altura[3] = altura[3] + (freq*3 * speed/4 * amplitude[3] * 20 * dt)
-
-    altura[1] = altura[1] - (freq*2 * speed/4 * amplitude[2] * 20 * dt) * 0.5
-
-    if love.keyboard.isDown("up") then
-        altura[1] = altura[1] + (freq * speed * amplitude[1] * 20 * dt)
-        altura[2] = altura[2] + (freq*2 * speed * amplitude[2] * 20 * dt)
-        altura[3] = altura[3] + (freq*3 * speed * amplitude[3] * 20 * dt)
+    if player["color"] == "red" then
+        player["pos_y"] = player["pos_y"] - 1
     end
 
-    if love.keyboard.isDown("down") then
-        altura[1] = altura[1] - (freq * speed * amplitude[1] * 20 * dt)
-        altura[2] = altura[2] - (freq*2 * speed * amplitude[2] * 20 * dt)
-        altura[3] = altura[3] - (freq*3 * speed * amplitude[3] * 20 * dt)
+    if player["color"] == "green" then
+        -- player["pos_y"] = player["pos_y"] + 0
     end
+
+    if player["color"] == "blue" then
+        player["pos_y"] = player["pos_y"] + 1
+    end
+
 end
 
+--====================================
+
 function love.draw()
-    for y=0,600 do
 
-        if y == math.floor(altura[1]) then
-            radius = 10
-        else
-            radius = 2
+    for pos_y = -200, 800 do
+
+        for wave_color, wave in pairs(waves) do
+
+            pos_x = wave_point(pos_y, wave["speed"], wave["freq"], wave["amplitude"])
+
+            set_color(wave["color"])
+
+            love.graphics.circle( "fill", pos_x + wave["offset_x"], pos_y + wave["offset_y"], 2)
+
+            if pos_y == player["pos_y"] then
+
+                if wave_color == player["color"] then
+                    love.graphics.circle( "fill", pos_x + wave["offset_x"], pos_y + wave["offset_y"], 10)
+                end
+            end
         end
-
-        love.graphics.setColor(255,0,0,127)
-        love.graphics.circle( "fill", (math.sin(maintimer*speed+freq*y)*amplitude[1])+offset_y[1], height-y,radius)
-
-        if y == math.floor(altura[2]) then
-            radius = 10
-        else
-            radius = 2
-        end
-
-        love.graphics.setColor(0,255,0,127)
-        love.graphics.circle( "fill", (math.sin(4+maintimer*speed+freq/2*y)*amplitude[2])+offset_y[2], height-y,radius)
-        -- love.graphics.circle( "fill", (math.sin(4+maintimer*speed+freq*y)*amplitude[2])+offset_y[2], height-y,radius)
-
-        if y == math.floor(altura[3]) then
-            radius = 10
-        else
-            radius = 2
-        end
-
-        love.graphics.setColor(0,0,255,255)
-        love.graphics.circle( "fill", (math.sin(maintimer*speed+freq/3*y)*amplitude[3])+offset_y[3], height-y,radius)
-        -- love.graphics.circle( "fill", (math.sin(maintimer*speed+freq*y)*amplitude[3])+offset_y[3], height-y,radius)
     end
 
     love.graphics.setColor(255,255,255,127)
