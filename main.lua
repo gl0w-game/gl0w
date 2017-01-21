78a370da4a45bef570d0d7a7a524bd267d2ddfa5
mariano.aquino <mariano.aquino@gmail.com>
2017-01-21 15:31:48 -0300

    testing collisions

--- main.lua
+++ main.lua
@@ -26,11 +26,11 @@ RED = "FF4136FF"
 GREEN = "2ECC40FF"
 BLUE = "0074D9FF"
 YELLOW = "2ECCFFFF"
+WHITE = "FFFFFFFF"
 
-
-alter_freq = 0.0001
-alter_amplitude = 0.1
-alter_timer=0
+-- alter_freq = 0.0001
+-- alter_amplitude = 0.1
+-- alter_timer=0
 
 function set_color(color)
     love.graphics.setColor(convertHex(color))
@@ -39,33 +39,28 @@ end
 --====================================
 -- WAVES
 
+function Wave(color, offset_x, offset_y, speed, freq, amplitude)
+    wave = {}
+    wave.color = color
+    wave.offset_x = offset_x
+    wave.offset_y = offset_y
+    wave.speed = speed
+    wave.freq = freq
+    wave.amplitude = amplitude
+    wave.pos_player_x = 0
+    -- waves[RED]["estado"] = 0
+    return wave
+end
+
 waves = {}
-waves["red"] = {}
-waves["red"]["color"] = RED
-waves["red"]["offset_x"] = 250
-waves["red"]["offset_y"] = 0
-waves["red"]["speed"] = 7
-waves["red"]["freq"] = 0.015
-waves["red"]["amplitude"] = 160
-waves["red"]["estado"] = 0
 
-waves["green"] = {}
-waves["green"]["color"] = GREEN
-waves["green"]["offset_x"] = 400
-waves["green"]["offset_y"] = 60
-waves["green"]["speed"] = 7
-waves["green"]["freq"] = 0.020
-waves["green"]["amplitude"] = 120
-waves["green"]["estado"] = 0
+waves[RED] = Wave(RED, 170, 0, 7, 0.05, 80)
+waves[GREEN] = Wave(GREEN, 300, 60, 7, 0.05, 120)
+waves[BLUE] = Wave(BLUE, 520, 0, 7, 0.05, 180)
 
-waves["blue"] = {}
-waves["blue"]["color"] = BLUE
-waves["blue"]["offset_x"] = 550
-waves["blue"]["offset_y"] = 0
-waves["blue"]["speed"] = 7
-waves["blue"]["freq"] = 0.025
-waves["blue"]["amplitude"] = 180
-waves["blue"]["estado"] = 0
+-- waves[RED] = Wave(RED, 170, 0, 0.7, 0.05, 80)
+-- waves[GREEN] = Wave(GREEN, 300, 60, 0.7, 0.05, 120)
+-- waves[BLUE] = Wave(BLUE, 520, 0, 0.7, 0.05, 180)
 
 background = love.graphics.newImage("gamebg.png")
 foreground = love.graphics.newImage("screenoverlay.png")
@@ -78,7 +73,7 @@ function wave_point(pos_y, speed, freq, amplitude)
 end
 
 function change_wave_offset_y(wave, inc)
-    waves[wave]["offset_y"] = waves[wave]["offset_y"] + inc
+    waves[wave].offset_y = waves[wave].offset_y + inc
 end
 
 --====================================
@@ -88,16 +83,16 @@ end
 LAST_STATE = 3
 
 player = {}
-player["pos_y"] = 300
-player["color"] = "red"
+player.pos_y = 300
+player.color = RED
 
 function change_player_color()
-    if player["color"] == "red" then
-        player["color"] = "green"
-    elseif player["color"] == "green" then
-        player["color"] = "blue"
-    elseif player["color"] == "blue" then
-        player["color"] = "red"
+    if player.color == RED then
+        player.color = GREEN
+    elseif player.color == GREEN then
+        player.color = BLUE
+    elseif player.color == BLUE then
+        player.color = RED
     end
 end
 
@@ -122,46 +117,46 @@ end
 
 function love.update(dt)
     maintimer = maintimer + dt
-    
-    alter_timer = alter_timer+dt
-    if alter_timer > 0.1 then
-        alter_timer = alter_timer - 0.1
-        rnd = love.math.random(1,9)
-        if rnd == 1 then
-            waves["red"]["estado"] = 1
-        elseif rnd == 2 then
-            waves["red"]["estado"] = 0
-        elseif rnd == 3 then
-            waves["red"]["estado"] = -1
-        elseif rnd == 4 then
-            waves["green"]["estado"] = 1
-        elseif rnd == 5 then
-            waves["green"]["estado"] = 0
-        elseif rnd == 6 then
-            waves["green"]["estado"] = -1
-        elseif rnd == 7 then
-            waves["blue"]["estado"] = 1
-        elseif rnd == 8 then
-            waves["blue"]["estado"] = 0
-        elseif rnd == 9 then
-            waves["blue"]["estado"] = -1
-        end
 
-        waves["red"]["freq"] = waves["red"]["freq"] + (alter_freq*waves["red"]["estado"]*dt*100)
-        waves["green"]["freq"] = waves["green"]["freq"] + (alter_freq*waves["green"]["estado"]*dt*100)
-        waves["blue"]["freq"] = waves["blue"]["freq"] + (alter_freq*waves["blue"]["estado"]*dt*100)
-        waves["red"]["amplitude"] = waves["red"]["amplitude"] + (alter_amplitude*waves["red"]["estado"]*dt*100)
-        waves["green"]["amplitude"] = waves["green"]["amplitude"] + (alter_amplitude*waves["green"]["estado"]*dt*100)
-        waves["blue"]["amplitude"] = waves["blue"]["amplitude"] + (alter_amplitude*waves["blue"]["estado"]*dt*100)
-        
+    -- alter_timer = alter_timer+dt
+    -- if alter_timer > 0.1 then
+    --     alter_timer = alter_timer - 0.1
+    --     rnd = love.math.random(1,9)
+    --     if rnd == 1 then
+    --         waves[RED]["estado"] = 1
+    --     elseif rnd == 2 then
+    --         waves[RED]["estado"] = 0
+    --     elseif rnd == 3 then
+    --         waves[RED]["estado"] = -1
+    --     elseif rnd == 4 then
+    --         waves[GREEN]["estado"] = 1
+    --     elseif rnd == 5 then
+    --         waves[GREEN]["estado"] = 0
+    --     elseif rnd == 6 then
+    --         waves[GREEN]["estado"] = -1
+    --     elseif rnd == 7 then
+    --         waves[BLUE]["estado"] = 1
+    --     elseif rnd == 8 then
+    --         waves[BLUE]["estado"] = 0
+    --     elseif rnd == 9 then
+    --         waves[BLUE]["estado"] = -1
+    --     end
+
+    --     waves[RED]["freq"] = waves[RED]["freq"] + (alter_freq*waves[RED]["estado"]*dt*100)
+    --     waves[GREEN]["freq"] = waves[GREEN]["freq"] + (alter_freq*waves[GREEN]["estado"]*dt*100)
+    --     waves[BLUE]["freq"] = waves[BLUE]["freq"] + (alter_freq*waves[BLUE]["estado"]*dt*100)
+    --     waves[RED]["amplitude"] = waves[RED]["amplitude"] + (alter_amplitude*waves[RED]["estado"]*dt*100)
+    --     waves[GREEN]["amplitude"] = waves[GREEN]["amplitude"] + (alter_amplitude*waves[GREEN]["estado"]*dt*100)
+    --     waves[BLUE]["amplitude"] = waves[BLUE]["amplitude"] + (alter_amplitude*waves[BLUE]["estado"]*dt*100)
+
+    -- end
+
+    if player.color == RED then
+        player.pos_y = player.pos_y - dt*100
     end
 
-    if player["color"] == "red" then
-        player["pos_y"] = player["pos_y"] - dt*4
-    end
-
-    if player["color"] == "blue" then
-        player["pos_y"] = player["pos_y"] + dt*4
+    if player.color == BLUE then
+        player.pos_y = player.pos_y + dt*100
     end
 
     background_y = background_y + background_spd * dt
@@ -169,80 +164,115 @@ function love.update(dt)
         background_y = background_y - love.graphics.getHeight()
     end
 
-    print(waves["blue"]["freq"].." - "..waves["blue"]["amplitude"])
-
 end
 
---====================================
+--=============================================================
 
-
-function love.load()
+-- function love.load()
     -- love.window.setMode(800, 600, {resizable=false, vsync=true, fullscreentype="exclusive"})
     -- love.window.setFullscreen(true, "exclusive")
 
     -- you can also provide parameters on effect construction
-    gr = shine.godsray({exposure=25, decay=0.95})
-    gr2 = shine.godsray()
 
-    scanlines = shine.scanlines({pixel_size=5})
-    crt = shine.crt()
-    oldtv = scanlines:chain(crt)
+    -- gr = shine.godsray({exposure=25, decay=0.95})
+    -- gr2 = shine.godsray()
+
+    -- scanlines = shine.scanlines({pixel_size=5})
+    -- crt = shine.crt()
+    -- oldtv = scanlines:chain(crt)
+
     -- more code here
-end
-
+-- end
 
 function drawstuff()
-    for pos_y = -200, 800 do       
+    for pos_y = -200, 800 do
+
         for wave_color, wave in pairs(waves) do
-            pos_x = wave_point(pos_y, wave["speed"], wave["freq"], wave["amplitude"])
-            set_color(wave["color"])
-            love.graphics.circle( "fill", pos_x + wave["offset_x"], pos_y + wave["offset_y"], 2)
-            if pos_y == math.floor(player["pos_y"]) then
-                if wave_color == player["color"] then
-                        set_color(wave["color"])
-                        love.graphics.circle( "fill", pos_x + wave["offset_x"], pos_y + wave["offset_y"], 18)
-                        love.graphics.setColor(255,255,255,255)
-                        love.graphics.circle( "fill", pos_x + wave["offset_x"], pos_y + wave["offset_y"], 14)
-                        set_color(wave["color"])
-                        love.graphics.circle( "fill", pos_x + wave["offset_x"], pos_y + wave["offset_y"], 8)
+            pos_x = wave_point(pos_y, wave.speed, wave.freq, wave.amplitude) -- pos_x = wave.pos_player_x --
+            set_color(wave.color)
+            love.graphics.circle( "fill", pos_x + wave.offset_x, pos_y + wave.offset_y, 2)
+
+            if pos_y == math.floor(player.pos_y) then
+
+                wave.pos_player_x = math.floor(pos_x)
+
+                if wave_color == player.color then
+                        set_color(wave.color)
+                        love.graphics.circle( "fill", pos_x + wave.offset_x, pos_y + wave.offset_y, 18)
+                        set_color(WHITE)
+                        love.graphics.circle( "fill", pos_x + wave.offset_x, pos_y + wave.offset_y, 14)
+                        set_color(wave.color)
+                        love.graphics.circle( "fill", pos_x + wave.offset_x, pos_y + wave.offset_y, 8)
                 end
             end
         end
     end
-    love.graphics.setColor(255,255,255,127)
+
+    if waves[RED].pos_player_x == waves[GREEN].pos_player_x then
+        love.graphics.print("collision 1-2!", 0,50)
+        -- if player["color"] == colors[1] then
+        --     player["color"] = colors[2]
+        -- else
+        --     player["color"] = colors[1]
+        -- end
+    end
+
+    if waves[RED].pos_player_x == waves[BLUE].pos_player_x then
+        love.graphics.print("collision 1-3!", 0,50)
+        -- if player["color"] == colors[1] then
+        --     player["color"] = colors[3]
+        -- else
+        --     player["color"] = colors[1]
+        -- end
+    end
+
+    if waves[GREEN].pos_player_x == waves[BLUE].pos_player_x then
+        love.graphics.print("collision 2-3!", 0,50)
+        -- if player["color"] == colors[2] then
+        --     player["color"] = colors[3]
+        -- else
+        --     player["color"] = colors[2]
+        -- end
+    end
+
+
+    set_color(WHITE)
     love.graphics.print("T:"..maintimer,0,0)
 end
-function drawactivelane()
-    for pos_y = -200, 800 do
-        for wave_color, wave in pairs(waves) do
 
-            pos_x = wave_point(pos_y, wave["speed"], wave["freq"], wave["amplitude"])
-            if wave_color == player["color"] then
+-- function drawactivelane()
+--     for pos_y = -200, 800 do
+--         for wave_color, wave in pairs(waves) do
 
-            set_color(wave["color"])
-            love.graphics.circle( "fill", pos_x + wave["offset_x"], pos_y + wave["offset_y"], 2)
-            end
-        end
-    end
-end
+--             pos_x = wave_point(pos_y, wave.speed, wave.freq, wave.amplitude)
+--             if wave_color == player.color then
+
+--             set_color(wave.color)
+--             love.graphics.circle( "fill", pos_x + wave.offset_x, pos_y + wave.offset_y, 2)
+--             end
+--         end
+--     end
+-- end
 
 function love.draw()
-        oldtv:draw(function()
 
-        	love.graphics.setBlendMode("alpha") --Default blend mode
-        	love.graphics.setBlendMode("add") --Default blend mode
+    -- oldtv:draw(function()
 
-            gr:draw(function()
-                drawactivelane()
-            end)
+    --     love.graphics.setBlendMode("alpha") --Default blend mode
+    --     love.graphics.setBlendMode("add") --Default blend mode
 
-            love.graphics.draw(background, 0, background_y) 
-            love.graphics.draw(background, 0, background_y - background:getHeight())
+    --     gr:draw(function()
+    --         drawactivelane()
+    --     end)
 
-            gr2:draw(function()
-                drawstuff()
-            end)
+    love.graphics.draw(background, 0, background_y)
+    love.graphics.draw(background, 0, background_y - background:getHeight())
 
-            drawstuff()
-        end)
+    --     gr2:draw(function()
+    --         drawstuff()
+    --     end)
+
+    --     drawstuff()
+    -- end)
+    drawstuff()
 end
